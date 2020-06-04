ceb8f928adb7420f83ebbe5a18615738fe50174c
Oleg Pustovit <ursus95@gmail.com>
2020-06-04 19:31:31 +0300

    fixed layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,32 +15,17 @@ import Boxes from '../components/3d/boxes'
 import * as THREE from 'three'
 import Effects from '../components/3d/effects'
 
-import { disableBodyScroll } from 'body-scroll-lock'
-
-disableBodyScroll(document)
-
-
 
 class IndexPage extends React.Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    }
-
-    window.addEventListener('resize', (e) => this.setState({
-      width: e.target.innerWidth,
-      height: e.target.innerHeight,
-    }));
-
   }
+
   render() {
-    return <div style={{ overflow: 'hidden', maxHeight: this.state.height }}>
+    return <div style={{ overflow: 'hidden', maxHeight: '100%' }}>
       <Fade delay={200}>
         <Canvas className='uc-boxes'
-          style={{ width: '50%', height: this.state.height, position:'absolute' }}
+          style={{ width: '50%', height: '100%', position: 'absolute' }}
           gl={{ antialias: true, alpha: true }}
           camera={{ position: [0, 0, 15], fov: 33, near: 5, far: 20 }}
           onCreated={({ gl }) => gl.setClearColor(0x000000, 0)}
@@ -53,23 +38,24 @@ class IndexPage extends React.Component {
       </Fade>
       <Layout>
         <SEO title="Home" />
-        <div className="uc-flex-container" style={{width: '90%', height: this.state.height}}>
-        <h1 className="uc-big-title">Crystal Boxes</h1>
-        <Fade left>
-          <div className="uc-social-network-icons">
-            {socialIcon('email', 'mailto:crystalboxesgfx@gmail.com')}
-            {socialIcon('instagram', 'https://instagram.com/crystalboxes')}
-            {socialIcon('twitter', 'https://twitter.com/crystalboxes')}
-            {socialIcon('behance', 'https://www.behance.net/crystalboxes')}
-          </div>
-        </Fade>
-        <Fade left distance="80px">
-          <div className="uc-under-construction">under construction</div>
-        </Fade>
+        <div className="uc-flex-container">
+          <div style={{height:'100px'}}></div>
+          <h1 className="uc-big-title">Crystal Boxes</h1>
+          <Fade left>
+            <div className="uc-social-network-icons">
+              {socialIcon('email', 'mailto:crystalboxesgfx@gmail.com')}
+              {socialIcon('instagram', 'https://instagram.com/crystalboxes')}
+              {socialIcon('twitter', 'https://twitter.com/crystalboxes')}
+              {socialIcon('behance', 'https://www.behance.net/crystalboxes')}
+            </div>
+          </Fade>
+          <Fade left distance="80px">
+            <div className="uc-under-construction">under construction</div>
+          </Fade>
         </div>
 
-      </Layout>
-    </div>
+      </Layout >
+    </div >
   }
 }
 
